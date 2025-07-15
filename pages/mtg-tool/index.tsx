6cf6f70eaa902f2bae401c68c0722520ea4d7db7
Eddie Tindame <eddie.tindame@googlemail.com>
2025-07-15 22:15:02 +0100

    feat: mtg-tool

---
+++ pages/mtg-tool/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Layout } from 'components/Layout'
+import { MTGTool } from 'components/MTGTool/index'
+
+export default function MTGToolPage() {
+  return (
+    <div className="h-screen bg-gradient-to-br from-green-900 via-green-800 to-green-700">
+      <div className="flex h-full flex-col">
+        <div className="flex-1">
+          <MTGTool />
+        </div>
+      </div>
+    </div>
+  )
+}
