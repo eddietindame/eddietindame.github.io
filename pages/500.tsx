082a953a2551bc86dc08a82e8775bd41c2f2c666
Eddie Tindame <eddie.tindame@googlemail.com>
2025-07-15 23:22:21 +0100

    feat: dynamic screen size

--- pages/500.tsx
+++ pages/500.tsx
@@ -8,7 +8,7 @@ export default function Custom500() {
       <Head>
         <title>Eddie Tindame | 500 | Internal server error</title>
       </Head>
-      <Layout className="h-screen">
+      <Layout className="dynamic-screen">
         <div className="flex flex-1 items-center justify-center">
           <h1 className="text-3xl">500 - Aw hell naw.</h1>
         </div>
